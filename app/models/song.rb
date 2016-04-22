b5797a68e5f1296ffa31f9d50a44c3bdd7059110
Jordan Holliday <jordanholliday@gmail.com>
2016-04-22 14:13:54 -0400

    Add dependent destroy to Song::has_many(:beats)

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,11 @@
 class Song < ActiveRecord::Base
   validates :name, :youtube_id, presence: true
 
-  has_many :beats
+  has_many :beats, dependent: :destroy
 
   def bpm
     song_beats = self.beats.sort
-    # total beat count divided by time from first beat to last beat
+    # bpm = total beat count divided by time btwn first beat & last
     song_beats.count / ((song_beats.last.time - song_beats.first.time) / 60)
   end
 end
